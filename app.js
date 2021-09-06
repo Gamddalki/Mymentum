51a9f622c2e6507e0eb5f895eb75d466cfd7fe58
Gamddalki <hannahk01@naver.com>
2021-09-06 17:42:51 +0900

    Create login form

---
+++ app.js
@@ -0,0 +1,6 @@
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function loginBtnClick() {}
+
+loginButton.addEventListener("click", loginBtnClick);
