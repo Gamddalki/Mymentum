c1f7e87fe2fda14bfef09926b8bdb8004ad42563
Gamddalki <hannahk01@naver.com>
2021-09-06 18:47:20 +0900

    Saving and Loading username

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const grettings = document.querySelector("#greeting");
 
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+} else {
+  showGreetings(savedUsername);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASS);
-  grettings.innerText = `Hello ${username}!`;
+  showGreetings(username);
+}
+
+function showGreetings(username) {
+  grettings.innerText = `Hello, ${username}!`;
   grettings.classList.remove(HIDDEN_CLASS);
 }
 
