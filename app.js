1348428639102e3cc10211f7ff6e224eee943534
Gamddalki <hannahk01@naver.com>
2021-09-06 18:33:05 +0900

    Say hello to user

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const grettings = document.querySelector("#greeting");
+
+const HIDDEN_CLASS = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASS);
+  grettings.innerText = `Hello ${username}!`;
+  grettings.classList.remove(HIDDEN_CLASS);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
