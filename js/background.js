e95e4fe589a7877816fe7714be15291e0c5956b6
Gamddalki <hannahk01@naver.com>
2021-09-07 15:30:40 +0900

    Update style.css

--- js/background.js
+++ js/background.js
@@ -23,6 +23,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-bgImage.id = "background";
 
-document.body.appendChild(bgImage);
+document.body.style = `background-image:url(img/${chosenImage}); background-size:cover;`;
