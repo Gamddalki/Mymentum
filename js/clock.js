7b6b017abbf263d0fb82c7e3439b27af51203ea5
Gamddalki <hannahk01@naver.com>
2021-09-07 12:31:29 +0900

    Add settings of clock

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,25 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  let ampm = "AM";
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  if (hours === 0) {
+    ampm = "AM";
+    hours += 12;
+  } else if (hours === 12) {
+    ampm = "PM";
+  } else if (hours > 12) {
+    hours -= 12;
+  }
+
+  String(hours).padStart(2, "0");
+  String(minutes).padStart(2, "0");
+  String(seconds).padStart(2, "0");
+
+  clock.innerText = `${ampm} ${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
