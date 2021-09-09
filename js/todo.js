6a1f8020651c0d609e07f8298e58d1c46965e081
Gamddalki <hannahk01@naver.com>
2021-09-09 15:06:13 +0900

    Add buttons to todo-list

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,37 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const todos = [];
+
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  todos.push(newTodo);
   paintTodo(newTodo);
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
+  const don = document.createElement("button");
+  const del = document.createElement("button");
   span.innerText = newTodo;
+  don.innerText = "✔";
+  don.addEventListener("click", donTodo);
+  del.innerText = "❌";
+  del.addEventListener("click", delTodo);
+  li.appendChild(span);
+  li.appendChild(don);
+  li.appendChild(del);
   todoList.appendChild(li);
+}
+
+function donTodo(event) {}
+
+function delTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
