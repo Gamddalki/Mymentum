1e8121dce107bdcd16cc8afe7e2c685908e5a49d
Gamddalki <hannahk01@naver.com>
2021-09-09 14:19:22 +0900

    Create todo-list

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,18 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event) {
+function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+}
+
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoList.appendChild(newTodo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
